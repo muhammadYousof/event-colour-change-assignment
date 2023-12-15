7afa06d8d7553f60b0e316d3fcb2801b4697b62e
muhammadYousof <my 7370044@gmail.com>
2023-12-15 06:27:46 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// function our(){
+//     var pera = document.getElementById('pera')
+//     pera.style.color = 'pink'
+//     pera.style.fontSize = '30px'
+//     pera.style.textShadow = '0px 0px 5px black'
+//     pera.style.border = '3px solid grey'
+//     pera.style.borderRadius = '20px'
+// }
+
+
+// function out(){
+//     var pera = document.getElementById('pera')
+//     pera.style.color = 'blue'
+//     pera.style.fontSize = '40px'
+//     pera.style.textShadow = '0px 0px 5px darkgrey'
+//     pera.style.border = '6px solid grey'
+//     pera.style.borderRadius = '50px'
+// }
+
+
+
+function btn(pera){
+    document.getElementById('pera').style.color = "yellow"
+}
+
+function btn1(pera1){
+    document.getElementById('pera1').style.color = "maroon"
+}
+
+function btn2(pera2){
+    document.getElementById('pera2').style.color = "red"
+}
